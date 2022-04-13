118fb5148f77fec877bb8929855229800b706386
Fazliddin Umirzoqov <fazliddinumirzoqov0122@gmail.com>
2022-04-13 18:16:49 +0500

    mm

---
+++ src/js/controller.js
@@ -0,0 +1,35 @@
+const { async } = require('regenerator-runtime');
+
+import * as model from './model.js';
+import recipeViev from './views/recipeView.js';
+
+// const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    // console.log(id);
+    recipeViev.spinner();
+    await Promise.race([timeout(2), model.loadRecipe(id)]);
+
+    recipeViev.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+window.addEventListener('hashchange', showRecipe);
+window.addEventListener('load', showRecipe);
